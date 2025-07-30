7a9512c650d7ae5cef30c4cf1e36624e1d2ca79a
rahulboss905 <166490010+rahulboss905@users.noreply.github.com>
2025-07-30 07:53:12 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,13 +1,3 @@
 import os
-from flask import Flask
-from flask_restful import Resource, Api
-
-app = Flask(__name__)
-api = Api(app)
-
-class Greeting (Resource):
-    def get(self):
-        return "RAUSHAN Userbot is Up & Running!"
-
-api.add_resource(Greeting, '/')
-app.run(host="0.0.0.0", port=os.environ.get("PORT", 8080))
+port = int(os.environ.get("PORT", 8000))  # Render provides $PORT
+app.run(host="0.0.0.0", port=port)
